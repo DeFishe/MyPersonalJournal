3c3d248ccf07d4f0ce496ea2b2967076ae4ec0c2
Darian Fisher <43389055+DeFishe@users.noreply.github.com>
2019-01-30 21:25:04 -0500

    Add files via upload

---
+++ PersonalJournal/PersonalJournal/JournalClassLibrary.NETFramework/JournalClassLibrary.NETFramework.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JournalClassLibrary.NETFramework
+{
+    public static class UserInfo
+    {
+        public static int userID = 0;
+        public static string userName = " ";
+    }
+
+    public class Login
+    {
+        public bool LoginProcess(string username, string password)
+        {
+            string usernameQuery = "select count (*) as cnt from USERS where USERNAME=@user and PASSWORD=@password";
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = @"Data Source=(local)\SQLEXPRESS; Initial Catalog =JOURNAL APP; Integrated Security=SSPI";
+            SqlCommand sqlCommand = new SqlCommand(usernameQuery, connection);
+            sqlCommand.Parameters.Clear();
+            sqlCommand.Parameters.AddWithValue("@user", username);
+            sqlCommand.Parameters.AddWithValue("@password", password);
+            connection.Open();
+
+            if(sqlCommand.ExecuteScalar().ToString() == "1")
+            {
+                connection.Close();
+                return true;
+            }
+            else
+            {
+                connection.Close();
+                return false;
+            }
+        }
+    }
+    public class EditAccount
+    {
+        private string newPassword = "";
+
+        public void PasswordUpdate(string newPassword)
+        {
+            string passwordUpdateQuery = $"UPDATE USERS SET PASSWORD = '{newPassword}' WHERE USERID = {UserInfo.userID}";
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = @"Data Source=(local)\SQLEXPRESS; Initial Catalog =JOURNAL APP; Integrated Security=SSPI";
+            SqlCommand sqlCommand = new SqlCommand(passwordUpdateQuery, connection);
+            connection.Open();
+            sqlCommand.ExecuteNonQuery();
+            connection.Close();
+        }
+    }
+    public class JournalList
+    {
+        public void GetTitlePreview(int userID, listbox)
+        {
+            string titlePreviewQuery = $"SELECT * FROM JOURNALS WHERE USERID = {UserInfo.userID}";
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = @"Data Source = (local)\SQLEXPRESS; Initial Catalog =JOURNAL APP; Integrated Security=SSPI";
+            SqlCommand sqlCommand = new SqlCommand(titlePreviewQuery, connection);
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+            connection.Open();
+            while (sqlDataReader.Read())
+            {
+                listbox.Items.Add(sqlDataReader["JOURNALTITLE"]);
+            }
+        }
+    }
+    public class ViewJournal
+    {
+        int entryIDGlobal;
+        public ViewJournal(int entryID)
+        {
+            entryIDGlobal = entryID;
+        }
+
+        public string GetJournalBody(int entryID)
+        {
+            string journalBody = " ";
+            string getJournalBodyQuery = $"SELECT JOURNAL JOURNALENTRY FROM JOURNALS WHERE ENTRYID = {entryIDGlobal}";
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = @"Data Source = (local)\SQLEXPRESS; Initial Catalog =JOURNALAPP; Integrated Security=SSPI";
+            SqlCommand command = new SqlCommand(getJournalBodyQuery, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            journalBody = reader.ToString();
+            connection.Close();
+            return journalBody;
+        }
+    }
+    public class SaveJournal
+    {
+        string title = " ";
+        string body = " ";
+        public void UploadJournal(string title, string body)
+        {
+            //TODO send jounral to database
+        }
+    }
+}
---
+++ PersonalJournal/PersonalJournal/PersonalJournal/JOURNAL ENTRY.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PersonalJournal
+{
+    /// <summary>
+    /// Interaction logic for JOURNAL_ENTRY.xaml
+    /// </summary>
+    public partial class JOURNAL_ENTRY : Window
+    {
+        public JOURNAL_ENTRY()
+        {
+            InitializeComponent();
+        }
+    }
+}
